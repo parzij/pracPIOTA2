7b03d3a37befea45c7be82f4399003d039348540
parzi <batychkov@sfedu.ru>
2025-04-13 13:21:42 +0300

    new veersion main

--- main.cpp
+++ main.cpp
@@ -1,17 +1,19 @@
 /*************************************************************
  * ДЗ-2 Примитивная рекурсия (Вариант 2.10)
  *
- * Студент: Батычков Вячеслав Геннадьевич, Группа: КТбо1-7
+ * Студент: Батычков Вячеслаав Геннадьевич, КТбо1-7
  *
- * Программа: Реализует вычисление функции f(n, x),
+ * Программа: Реализует вычисление функции f(x1, x2),
  * полученной из базовых функций:
- *   g(x) = x 
+ *   g(x) = x
  *   h(x, y, z) = x + y
- * по схеме примитивной рекурсии.
+ * по схеме примитивной рекурсии:
+ *   1) f(0, x2) = x2
+ *   2) f(x1 + 1, x2) = h(x1, f(x1, x2), x2) = x1 + f(x1, x2)
  *
- * Используются два способа:
- * 1) Рекурсивное определение (recurFunc).
- * 2) Аналитическая формула (analiticFunc).
+ * Аналитическая формула (для x1 ≥ 0):
+ *   f(x1, x2) = x2 + (x1 * (x1 - 1)) / 2
+ *
  *************************************************************/
 
 #include <iostream>
@@ -25,38 +27,87 @@ using namespace std;
 // Вывод: занчение примитивно-рекурсивной функции
 int recurFunc(int, int);
   
-// Аналитическая формула F(n, x) = x + (n * (n - 1)) / 2
+// Аналитическая формула F(x1, x2) = x2 + (x1 * (x1 - 1)) / 2
 // Ввод: шаг и параметр
 // Вывод: значние по аналитической функции
 int analiticFunc(int, int);
 
 int main() {
     SetConsoleOutputCP(1251);
-    SetConsoleCP(1251);      
+    SetConsoleCP(1251);
     setlocale(LC_ALL, "Russian");
 
-    int n, x;
-    wcout << L"Введите n: ";
-    cin >> n;
-    wcout << L"Введите x: ";
-    cin >> x;
+    wcout << L"Схема рекурсии:" << endl;
+    wcout << L"1) f(0, x2) = x2" << endl;
+    wcout << L"2) f(x1 + 1, x2) = x1 + f(x1, x2)" << endl;
+    wcout << L"Аналитическая формула: f(x1, x2) = x2 + (x1 * (x1 - 1)) / 2" << endl << endl;
 
-    int recurResult    = recurFunc(n, x);
-    int analiticResult = analiticFunc(n, x);
+    bool keepWorking = true;
 
-    wcout << L"f(" << n << L", " << x << L") по рекурсии = " << recurResult << endl;
-    wcout << L"f(" << n << L", " << x << L") по аналитической формуле = " << analiticResult << endl;
+    while (keepWorking) {
+        int x1, x2;
+        bool flag = true;
 
-    (recurResult == analiticResult) ? wcout << L"YES" : wcout << L"NO";
-    wcout << endl;
+        wcout << L"Введите x1: ";
+        cin >> x1;
+        wcout << L"Введите x2: ";
+        cin >> x2;
+
+        if (x1 < 0) {
+            wcout << L"Ошибка: x1 должен быть неотрицательным." << endl;
+            wcout << L"Повторить ввод? (y/n): ";
+            char choice;
+            cin >> choice;
+            if (choice == 'n' || choice == 'N') {
+                wcout << L"Программа завершена." << endl;
+                keepWorking = false;
+            }
+            flag = false;
+        }
+
+        if (keepWorking && flag) {
+            wcout << endl << L"--- Рекурсивное вычисление ---" << endl;
+            int recurResult = recurFunc(x1, x2);
+
+            int analiticResult = analiticFunc(x1, x2);
+            wcout << endl << L"--- Сравнение с аналитической формулой ---" << endl;
+            wcout << L"f(" << x1 << L"," << x2 << L") по рекурсии = " 
+                  << recurResult << endl;
+            wcout << L"f(" << x1 << L"," << x2 << L") по аналитической формуле = " 
+                  << analiticResult << endl;
+
+            (recurResult == analiticResult) ? wcout << L"Результаты совпадают!" : wcout << L"Результаты НЕ совпадают!";
+            wcout << endl;
+
+            wcout << endl << L"Повторить ввод? (y/n): ";
+            char again; cin >> again;
+            if (again == 'n' || again == 'N') {
+                wcout << L"Программа завершена." << endl;
+                keepWorking = false;
+            }
+        }
+    }
     return 0;
 }
 
-int recurFunc(int n, int x) {
-    return (n == 0) ? x : ((n - 1) + recurFunc(n - 1, x));
+int recurFunc(int x1, int x2) {
+    int result;
+    if (x1 == 0) {
+        wcout << L"f(0," << x2 << L") = " << x2 << endl;
+        result = x2;
+    } else {
+        int prev = recurFunc(x1 - 1, x2);
+        result = (x1 - 1) + prev;
+        wcout << L"f(" << x1 << L"," << x2 << L") = h(" 
+              << (x1 - 1) << L", f(" << (x1 - 1) << L"," 
+              << x2 << L"), " << x2 << L") = h(" << (x1 - 1) 
+              << L"," << prev << L"," << x2 << L") = " 
+              << result << endl;
+    }
+    return result;
 }
 
 int analiticFunc(int n, int x) {
-    return x + (n * (n - 1)) / 2;
+    int result = x + (n * (n - 1)) / 2;
+    return result;
 }
-
